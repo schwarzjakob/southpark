de1b9901c392a44e76d700b32dcb8e1f1595e7da
Timon <T.Tirtey@campus.lmu.de>
2024-05-13 17:53:41 +0200

    Add basic MapView Framework Components

---
+++ frontend/src/components/MapComponent.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const MapComponent = () => {
+  return (
+    <div>
+      <h2>MapComponent</h2>
+    </div>
+  );
+};
+
+export default MapComponent;
--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
+import CalendarSlider from "./CalendarSlider.jsx";
+import MapComponent from "./MapComponent.jsx";
 
 const MapView = () => {
-  const [tableData, setTableData] = useState([]);
-
   return (
     <div>
       <h2>MapView</h2>
+      <div className="mapView-Container">
+        <div className="mapView__item label">Events</div>
+        <div className="mapView__item content">
+          <CalendarSlider />
+        </div>
+        <div className="mapView__item label">Parking Lots</div>
+        <div className="mapView__item content">
+          <CalendarSlider />
+        </div>
+        <div className="mapView__item label">Map</div>
+        <div className="mapView__item content">
+          <MapComponent />
+        </div>
+      </div>
     </div>
   );
 };
