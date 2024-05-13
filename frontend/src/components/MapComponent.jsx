ded0518dd48386b3489a5c1a873d9fca92485475
NickSchitt2510 <121604402+NickSchitt2510@users.noreply.github.com>
2024-05-13 23:16:05 +0200

    Readjust center and size of the map component and add bounds that doesn't work yet

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -4,8 +4,14 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 const MapComponent = () => {
+  // Bounds did not work. Still in progress.
+  const bounds = [
+    [48.133145, 11.688606], // Southwest coordinates
+    [48.33, 11.722], // Northeast coordinates
+  ];
+
   return (
-    <MapContainer center={[48.1375, 11.702]} zoom={16}>
+    <MapContainer center={[48.1375, 11.702]} zoom={16} bounds={bounds}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
