6b2b04adc8f408e3154d7e8ed34d4dd460885d9e
Timon <T.Tirtey@campus.lmu.de>
2024-05-29 00:48:05 +0200

    Refactor Team Page, Minor CSS changes

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -1,94 +1,127 @@
-// src/components/Team.jsx
-
 import React from "react";
-import { Box, Typography, Grid, Link } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  IconButton,
+  Link,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import "../styles/TeamComponent.css";
 import tolkien from "../assets/team/tolkien.jpeg";
 import eric from "../assets/team/eric.jpeg";
 import tuong from "../assets/team/tuong.jpeg";
 import kenny from "../assets/team/kenny.jpeg";
 
+const content = [
+  {
+    headline: "Meet our team",
+    description:
+      "Project SouthPark is a hands-on initiative that brings together Messe München and students from the MMT program at LMU Munich. The project's mission is to develop a practical MVP for optimizing parking space allocation during high-traffic events. This collaboration serves as a real-world learning opportunity for MMT students, while providing Messe München with innovative solutions to manage event logistics more effectively.",
+  },
+];
+
 const teamMembers = [
+  {
+    name: "Nichole Chen",
+    img: tuong,
+    position: "",
+    linkedIn: "https://www.linkedin.com/in/pei-lin-chen-658836184/",
+    email: "mailto:pei.chen@campus.lmu.de",
+  },
   {
     name: "Jakob Schwarz",
     img: tolkien,
-    linkedin: "https://www.linkedin.com/in/schwarzjakob/",
-    position: "Co-Founder, Head of Technology",
+    position: "",
+    linkedIn: "https://www.linkedin.com/in/schwarzjakob/",
+    email: "mailto:jakob.schwarz@campus.lmu.de",
   },
   {
     name: "Sven Tiefenthaler",
     img: eric,
-    linkedin: "mailto:s.tiefenthaler@campus.lmu.de",
-    position: "Co-Founder, Head of User Experience (UX)",
+    position: "",
+    linkedIn: "https://www.linkedin.com/in/sven-tiefenthaler-a7617221b/",
+    email: "mailto:s.tiefenthaler@campus.lmu.de",
   },
-  {
-    name: "Nichole Chen",
-    img: tuong,
-    linkedin: "https://www.linkedin.com/in/pei-lin-chen-658836184/",
-    position: "Co-Founder, Head of Frontend Development",
-  },
+
   {
     name: "Timon Tirtey",
     img: kenny,
-    linkedin: "https://de.linkedin.com/in/timontirtey/",
-    position: "Co-Founder, Head of Product",
+    position: "",
+    linkedIn: "https://www.linkedin.com/in/timontirtey/",
+    email: "mailto:t.sirtey@campus.lmu.de",
   },
 ];
 
-const Team = () => {
+const renderTeam = () => {
   return (
-    <Box sx={{ p: 2, textAlign: "center" }}>
-      <Typography variant="h1" component="h2" gutterBottom>
-        Meet the developer team
-      </Typography>
-      <Grid container spacing={4} justifyContent="center" sx={{ my: 4 }}>
-        {teamMembers.map((member) => (
-          <Grid item key={member.name} xs={12} sm={6} md={3}>
-            <Link
-              href={member.linkedin}
-              target="_blank"
-              rel="noopener noreferrer"
-              underline="none"
-            >
-              <Box
-                component="img"
-                src={member.img}
-                alt={member.name}
-                sx={{
-                  width: "300px",
-                  height: "300px",
-                  borderRadius: "50%",
-                  border: "5px solid var(--color-primary)",
-                }}
-              />
-              <Typography
-                variant="h6"
-                component="p"
-                mt={2}
-                color="var(--color-primary)"
-              >
-                {member.name}
-              </Typography>
-              <Typography variant="body1" component="p" mt={1}>
-                {member.position}
-              </Typography>
-            </Link>
-          </Grid>
-        ))}
+    <Container className="outer-container">
+      <Grid container className="inner-container">
+        <Grid item xs={12} md={4} className="headings-container">
+          <Typography variant="h2" className="heading-text">
+            {content[0].headline}
+          </Typography>
+
+          <Typography variant="body1" className="sub-heading-text">
+            {content[0].description}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          container
+          spacing={2}
+          className="team-members-container"
+        >
+          {teamMembers.map((member, index) => (
+            <Grid item xs={12} sm={6} key={index} className="card-container">
+              <Card className="card">
+                <CardMedia
+                  className="card-img"
+                  component="img"
+                  alt={member.name}
+                  height="250"
+                  image={member.img}
+                />
+                <CardContent className="content">
+                  <Typography variant="h6" className="name center">
+                    {member.name}
+                  </Typography>
+                  <div class="center">
+                    <IconButton
+                      component={Link}
+                      href={member.linkedIn}
+                      target="_blank"
+                      rel="noopener"
+                      className="icon-button"
+                    >
+                      <LinkedInIcon />
+                    </IconButton>
+                    <IconButton
+                      component={Link}
+                      href={member.email}
+                      className="icon-button"
+                    >
+                      <EmailIcon />
+                    </IconButton>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
-      <Typography variant="h2" component="h2" gutterBottom>
-        Project Southpark
-      </Typography>
-      <Typography variant="p" component="p" gutterBottom>
-        Project SouthPark is a hands-on initiative that brings together Messe
-        München and students from the MMT program at LMU Munich. The project's
-        mission is to develop a practical MVP for optimizing parking space
-        allocation during high-traffic events. This collaboration serves as a
-        real-world learning opportunity for MMT students, while providing Messe
-        München with innovative solutions to manage event logistics more
-        effectively.
-      </Typography>
-    </Box>
+    </Container>
   );
 };
 
-export default Team;
+function TeamComponent() {
+  return renderTeam();
+}
+
+export default TeamComponent;
