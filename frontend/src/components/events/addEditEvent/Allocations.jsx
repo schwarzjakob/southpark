4319d17b6c64a4310d8722511a4c8696bdfbecf2
Jakob Schwarz <jakob.schwarz97@gmail.com>
2024-07-03 13:30:47 +0200

    Add total row

--- frontend/src/components/events/addEditEvent/Allocations.jsx
+++ frontend/src/components/events/addEditEvent/Allocations.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
   Box,
@@ -22,6 +22,9 @@ import {
   FunctionsRounded as FunctionsRoundedIcon,
   Garage as GarageIcon,
   AccountTreeRounded as AccountTreeRoundedIcon,
+  ArrowCircleUpRounded as ArrowCircleUpRoundedIcon,
+  PlayCircleFilledRounded as PlayCircleFilledRoundedIcon,
+  ArrowCircleDownRounded as ArrowCircleDownRoundedIcon,
 } from "@mui/icons-material";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
@@ -35,9 +38,9 @@ const Allocations = ({ eventId, isEditingDemands }) => {
   };
 
   const [allocations, setAllocations] = useState([]);
+  const [demands, setDemands] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-
   const [order, setOrder] = useState("asc");
   const [orderBy, setOrderBy] = useState("date");
   const navigate = useNavigate();
@@ -47,9 +50,9 @@ const Allocations = ({ eventId, isEditingDemands }) => {
       try {
         const response = await axios.get(`/api/events/allocations/${eventId}`);
         if (response.status === 204) {
-          setAllocations([]);
+          setAllocations([{}]); // Add an empty row if no data is received
         } else {
-          setAllocations(response.data);
+          setAllocations(response.data.length ? response.data : [{}]);
         }
       } catch (error) {
         console.error("Error fetching allocations data:", error);
@@ -60,6 +63,23 @@ const Allocations = ({ eventId, isEditingDemands }) => {
     };
 
     fetchAllocations();
+  }, [eventId]);
+
+  useEffect(() => {
+    const fetchDemands = async () => {
+      try {
+        const response = await axios.get(`/api/events/demands/${eventId}`);
+        if (response.status === 204) {
+          setDemands([]);
+        } else {
+          setDemands(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching demands data:", error);
+      }
+    };
+
+    fetchDemands();
   }, [eventId]);
 
   const formatDate = (dateString) => {
@@ -88,7 +108,49 @@ const Allocations = ({ eventId, isEditingDemands }) => {
     }
   });
 
+  const getPhaseIcon = (phase) => {
+    switch (phase) {
+      case "assembly":
+        return <ArrowCircleUpRoundedIcon />;
+      case "runtime":
+        return <PlayCircleFilledRoundedIcon />;
+      case "disassembly":
+        return <ArrowCircleDownRoundedIcon />;
+      default:
+        return null;
+    }
+  };
+
+  const groupedAllocations = sortedAllocations.reduce((acc, allocation) => {
+    const demand = demands.find(
+      (d) => formatDate(d.date) === formatDate(allocation.date),
+    );
+    const phase = demand ? demand.status : "other"; // default to 'other' if no matching demand is found
+    if (!acc[phase]) {
+      acc[phase] = [];
+    }
+    acc[phase].push(allocation);
+    return acc;
+  }, {});
+
   const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
+
+  const totalRowStyle = {
+    position: "relative",
+    borderBottom: "2px solid #6a91ce",
+    paddingTop: "6px",
+  };
+
+  const doubleStrokeStyle = {
+    content: '""',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    height: "3px",
+    borderTop: "1px solid rgba(128, 128, 128, 0.5)",
+    borderBottom: "1px solid rgba(128, 128, 128, 0.5)",
+  };
 
   if (loading) {
     return (
@@ -197,7 +259,7 @@ const Allocations = ({ eventId, isEditingDemands }) => {
                     <Box className="header-icon-container__label-unit">
                       (= 3x Car Units)
                     </Box>
-                  </Box>{" "}
+                  </Box>
                 </Box>
               </TableCell>
               <TableCell>
@@ -229,41 +291,178 @@ const Allocations = ({ eventId, isEditingDemands }) => {
                     <Box className="header-icon-container__label-unit">
                       (Total Car Units)
                     </Box>
-                  </Box>{" "}
+                  </Box>
                 </Box>
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {sortedAllocations.map((allocation) => (
-              <TableRow
-                key={allocation.allocation_id}
-                hover
-                onClick={() =>
-                  navigate(`/parking_space/${allocation.parking_lot_id}`, {
-                    state: { selectedDate: allocation.date },
-                  })
-                }
-                style={{ cursor: "pointer" }}
-                className="allocation-table-row"
-                data-date={formatDate(allocation.date)}
-              >
-                <TableCell>{formatDate(allocation.date)}</TableCell>
-                <TableCell>
-                  <Box className="parking-lot-box">
-                    {capitalize(allocation.parking_lot_name)}
-                  </Box>
-                </TableCell>
-                <TableCell>{allocation.allocated_cars}</TableCell>
-                <TableCell>{allocation.allocated_buses}</TableCell>
-                <TableCell>{allocation.allocated_trucks}</TableCell>
-                <TableCell>
-                  {allocation.allocated_cars +
-                    allocation.allocated_buses * 3 +
-                    allocation.allocated_trucks * 4}
+            {allocations.length === 1 && !allocations[0].allocation_id ? (
+              <TableRow>
+                <TableCell colSpan={7} align="center">
+                  No allocations available.
                 </TableCell>
               </TableRow>
-            ))}
+            ) : (
+              ["assembly", "runtime", "disassembly"].map(
+                (phase, phaseIndex) => (
+                  <React.Fragment key={phase}>
+                    {groupedAllocations[phase]?.length > 0 && (
+                      <>
+                        <TableRow
+                          style={{
+                            backgroundColor: "#f0f0f0",
+                            borderTop:
+                              phaseIndex > 0 ? "2px solid #6a91ce" : "none",
+                          }}
+                        >
+                          <TableCell colSpan={7}>
+                            <Box display="flex" alignItems="center">
+                              {getPhaseIcon(phase)}
+                              <Typography
+                                variant="h6"
+                                style={{ marginLeft: "0.5rem" }}
+                              >
+                                {phase.charAt(0).toUpperCase() + phase.slice(1)}
+                              </Typography>
+                            </Box>
+                          </TableCell>
+                        </TableRow>
+                        {groupedAllocations[phase].reduce(
+                          (acc, allocation, index, array) => {
+                            const isLastRow = index === array.length - 1;
+                            const nextDate = array[index + 1]?.date;
+                            const shouldSumRow =
+                              isLastRow || allocation.date !== nextDate;
+
+                            acc.push(
+                              <TableRow
+                                key={allocation.allocation_id}
+                                hover
+                                onClick={() =>
+                                  navigate(
+                                    `/parking_space/${allocation.parking_lot_id}`,
+                                    {
+                                      state: { selectedDate: allocation.date },
+                                    },
+                                  )
+                                }
+                                style={{ cursor: "pointer" }}
+                                className="allocation-table-row"
+                                data-date={formatDate(allocation.date)}
+                              >
+                                <TableCell>
+                                  {formatDate(allocation.date)}
+                                </TableCell>
+                                <TableCell>
+                                  <Box className="parking-lot-box">
+                                    {capitalize(allocation.parking_lot_name)}
+                                  </Box>
+                                </TableCell>
+                                <TableCell>
+                                  {allocation.allocated_cars}
+                                </TableCell>
+                                <TableCell>
+                                  {allocation.allocated_buses}
+                                </TableCell>
+                                <TableCell>
+                                  {allocation.allocated_trucks}
+                                </TableCell>
+                                <TableCell>
+                                  {allocation.allocated_cars +
+                                    allocation.allocated_buses * 3 +
+                                    allocation.allocated_trucks * 4}
+                                </TableCell>
+                              </TableRow>,
+                            );
+
+                            if (shouldSumRow) {
+                              acc.push(
+                                <TableRow
+                                  key={`${allocation.date}-total`}
+                                  style={{
+                                    ...totalRowStyle,
+                                    borderBottom: isLastRow
+                                      ? "none"
+                                      : "2px solid #6a91ce",
+                                  }}
+                                >
+                                  <TableCell>
+                                    <Box style={doubleStrokeStyle}></Box>
+                                    <Box
+                                      style={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                      }}
+                                    >
+                                      <FunctionsRoundedIcon></FunctionsRoundedIcon>
+                                    </Box>
+                                  </TableCell>
+                                  <TableCell></TableCell>
+                                  <TableCell>
+                                    {groupedAllocations[phase]
+                                      .filter(
+                                        (alloc) =>
+                                          alloc.date === allocation.date,
+                                      )
+                                      .reduce(
+                                        (sum, alloc) =>
+                                          sum + alloc.allocated_cars,
+                                        0,
+                                      )}
+                                  </TableCell>
+                                  <TableCell>
+                                    {groupedAllocations[phase]
+                                      .filter(
+                                        (alloc) =>
+                                          alloc.date === allocation.date,
+                                      )
+                                      .reduce(
+                                        (sum, alloc) =>
+                                          sum + alloc.allocated_buses,
+                                        0,
+                                      )}
+                                  </TableCell>
+                                  <TableCell>
+                                    {groupedAllocations[phase]
+                                      .filter(
+                                        (alloc) =>
+                                          alloc.date === allocation.date,
+                                      )
+                                      .reduce(
+                                        (sum, alloc) =>
+                                          sum + alloc.allocated_trucks,
+                                        0,
+                                      )}
+                                  </TableCell>
+                                  <TableCell>
+                                    {groupedAllocations[phase]
+                                      .filter(
+                                        (alloc) =>
+                                          alloc.date === allocation.date,
+                                      )
+                                      .reduce(
+                                        (sum, alloc) =>
+                                          sum +
+                                          alloc.allocated_cars +
+                                          alloc.allocated_buses * 3 +
+                                          alloc.allocated_trucks * 4,
+                                        0,
+                                      )}
+                                  </TableCell>
+                                </TableRow>,
+                              );
+                            }
+                            return acc;
+                          },
+                          [],
+                        )}
+                      </>
+                    )}
+                  </React.Fragment>
+                ),
+              )
+            )}
           </TableBody>
         </Table>
       </TableContainer>
