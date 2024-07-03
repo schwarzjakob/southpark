08e35c35710328ce5e4f5e008b1308da59d8cee0
Timon <T.Tirtey@campus.lmu.de>
2024-07-04 00:36:07 +0200

    Halls Entrance SVG Component

--- frontend/src/components/events/HallEntranceIcons.jsx
+++ frontend/src/components/events/HallEntranceIcons.jsx
@@ -1,68 +1,208 @@
 import PropTypes from "prop-types";
 
+const getContrastingTextColor = (backgroundColor) => {
+  const hex = backgroundColor.replace("#", "");
+  const r = parseInt(hex.substr(0, 2), 16);
+  const g = parseInt(hex.substr(2, 2), 16);
+  const b = parseInt(hex.substr(4, 2), 16);
+  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+  return luminance > 0.5 ? "black" : "white";
+};
+
 const HallEntranceIcons = ({ color, ids }) => {
   const paths = [
-    { id: "A1", d: "M145 378L145 446L178 446L177 378z" },
-    { id: "A2", d: "M194 378L194 446L227 446L226 378z" },
-    { id: "A3", d: "M242 378L243 446L275 446L275 378z" },
-    { id: "A4", d: "M566 549L567 617L599 617L599 549z" }, // Added missing path
-    { id: "A5", d: "M344 378L345 446L377 446L377 378z" },
-    { id: "A6", d: "M393 378L394 446L426 446L425 378z" },
-    { id: "B1", d: "M144 280L144 347L177 347L176 280z" },
-    { id: "B2", d: "M193 280L193 347L226 347L225 280z" },
-    { id: "B3", d: "M241 280L242 347L274 347L274 280z" },
-    { id: "B4", d: "M294 280L294 347L327 347L327 280z" },
-    { id: "B5", d: "M343 280L344 347L376 347L376 280z" },
-    { id: "B6", d: "M392 280L392 346L425 346L424 280z" },
-    { id: "C1", d: "M143 209L144 271L176 271L175 209z" },
-    { id: "C2", d: "M192 209L192 271L225 271L224 209z" },
-    { id: "C3", d: "M241 209L241 271L274 271L273 209z" },
-    { id: "C4", d: "M294 209L294 271L326 271L326 209z" },
-    { id: "C5", d: "M342 209L343 271L375 271L375 209z" },
-    { id: "C6", d: "M391 209L392 271L424 271L423 209z" },
+    {
+      id: "A1",
+      d: "M143 376L143 448L180 448L180 376z",
+      centerX: 161,
+      centerY: 412,
+    },
+    {
+      id: "A2",
+      d: "M192 376L192 448L229 448L229 376z",
+      centerX: 210,
+      centerY: 412,
+    },
+    {
+      id: "A3",
+      d: "M240 376L240 448L277 448L277 376z",
+      centerX: 259,
+      centerY: 412,
+    },
+    {
+      id: "A4",
+      d: "M293 376L293 448L330 448L330 376z",
+      centerX: 313,
+      centerY: 412,
+    },
+    {
+      id: "A5",
+      d: "M342 376L342 448L379 448L379 376z",
+      centerX: 360,
+      centerY: 412,
+    },
+    {
+      id: "A6",
+      d: "M391 376L391 448L428 448L428 376z",
+      centerX: 409,
+      centerY: 412,
+    },
+    {
+      id: "B1",
+      d: "M142 278L142 349L179 349L179 278z",
+      centerX: 160,
+      centerY: 313,
+    },
+    {
+      id: "B2",
+      d: "M191 278L191 349L228 349L228 278z",
+      centerX: 209,
+      centerY: 313,
+    },
+    {
+      id: "B3",
+      d: "M239 278L239 349L276 349L276 278z",
+      centerX: 258,
+      centerY: 313,
+    },
+    {
+      id: "B4",
+      d: "M292 278L292 349L329 349L329 278z",
+      centerX: 310,
+      centerY: 313,
+    },
+    {
+      id: "B5",
+      d: "M341 278L341 349L378 349L378 278z",
+      centerX: 359,
+      centerY: 313,
+    },
+    {
+      id: "B6",
+      d: "M390 278L390 348L427 348L427 278z",
+      centerX: 408,
+      centerY: 313,
+    },
+    {
+      id: "C1",
+      d: "M141 207L141 273L178 273L178 207z",
+      centerX: 159,
+      centerY: 240,
+    },
+    {
+      id: "C2",
+      d: "M190 207L190 273L227 273L227 207z",
+      centerX: 208,
+      centerY: 240,
+    },
+    {
+      id: "C3",
+      d: "M239 207L239 273L276 273L276 207z",
+      centerX: 257,
+      centerY: 240,
+    },
+    {
+      id: "C4",
+      d: "M292 207L292 273L329 273L329 207z",
+      centerX: 310,
+      centerY: 240,
+    },
+    {
+      id: "C5",
+      d: "M340 207L340 273L377 273L377 207z",
+      centerX: 358,
+      centerY: 240,
+    },
+    {
+      id: "C6",
+      d: "M389 207L389 273L426 273L426 207z",
+      centerX: 407,
+      centerY: 240,
+    },
     {
       id: "West",
-      d: "M80 358L88 358L84 346L128 346L128 351L141 351L141 376L129 376L129 384L86 384L89 374L80 374z",
+      d: "M74 354L86 354L82 342L126 342L126 351L139 351L139 376L123 376L123 384L80 384L87 370L74 370z",
+      centerX: 108,
+      centerY: 366,
     },
-    { id: "North West", d: "M106 215L125 204L130 213L111 224z" },
-    { id: "North", d: "M275 163L275 199L291 199L291 163z" },
+    {
+      id: "North West",
+      d: "M104 213L127 202L132 215L109 226z",
+      centerX: 118,
+      centerY: 188,
+    },
+    {
+      id: "North",
+      d: "M273 161L273 201L293 201L293 161z",
+      centerX: 283,
+      centerY: 181,
+    },
     {
       id: "North East",
-      d: "M393 193L393 206L423 206L423 200L407 193z",
+      d: "M393 189L393 202L423 202L423 196L407 189z",
+      centerX: 408,
+      centerY: 176,
     },
     {
       id: "East",
-      d: "M425 354L439 354L439 342L471 342L466 354L473 354L473 370L466 370L471 380L440 380L440 370L425 370z",
+      d: "M423 352L441 352L441 340L473 340L464 352L475 352L475 372L464 372L473 382L442 382L442 372L423 372z",
+      centerX: 453,
+      centerY: 362,
     },
-    { id: "parking-lot-PN3", d: "M255 62L287 62L287 135L276 129L252 80z" },
-    { id: "parking-lot-PN4", d: "M290 62L338 62L338 149L290 136z" },
-    { id: "parking-lot-PN5", d: "M341 152L341 59L354 56L379 56L379 163z" },
-    { id: "parking-lot-PN6", d: "M382 56L382 164L430 181L430 56z" },
-    { id: "parking-lot-PN7", d: "M433 56L433 181L501 209L501 56z" },
-    { id: "parking-lot-PN8", d: "M504 56L504 211L527 218L527 56z" },
-    { id: "parking-lot-PN9", d: "M530 56L530 219L561 227L561 82L539 56z" },
-    { id: "parking-lot-PN10", d: "M564 86L564 227L588 231L613 233L613 169z" },
-    { id: "parking-lot-PN11", d: "M615 172L615 233L651 233z" },
-    { id: "parking-lot-PN12", d: "M586 119L654 232L677 230L700 225L625 119z" },
   ];
 
   return (
-    <svg width="200" height="90" viewBox="0 100 800 400">
+    <svg overflow="hidden" width="150" height="120" viewBox="76 180 400 240">
       <g>
         {paths.map((path) => (
-          <path
-            key={path.id}
-            className={`halls ${path.id}`}
-            stroke="#b3b3b3"
-            strokeOpacity="1"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            fill={ids.includes(path.id) ? color : "#cccccc"}
-            fillOpacity="1"
-            fillRule="evenodd"
-            d={path.d}
-          />
+          <g key={path.id}>
+            <path
+              className={`halls ${path.id}`}
+              stroke={ids.includes(path.id) ? color : "#cccccc"}
+              strokeOpacity="1"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill={ids.includes(path.id) ? color : "#cccccc"}
+              fillOpacity="1"
+              fillRule="evenodd"
+              d={path.d}
+            />
+            <text
+              x={path.centerX}
+              y={path.centerY}
+              textAnchor="middle"
+              fontSize="26px"
+              fontWeight="bold"
+              fill={
+                ids.includes(path.id) &&
+                path.id !== "North West" &&
+                path.id !== "North East"
+                  ? getContrastingTextColor(color)
+                  : "#484848"
+              }
+              dy=".3em"
+            >
+              {path.id === "North West"
+                ? "NW"
+                : path.id === "North East"
+                ? "NE"
+                : path.id === "South West"
+                ? "SW"
+                : path.id === "South East"
+                ? "SE"
+                : path.id === "North"
+                ? "N"
+                : path.id === "South"
+                ? "S"
+                : path.id === "East"
+                ? "E"
+                : path.id === "West"
+                ? "W"
+                : path.id}{" "}
+            </text>
+          </g>
         ))}
       </g>
     </svg>
